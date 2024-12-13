20e64f80fd8eae4cb46b3eb34752b55daa5979e0
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-12-13 05:13:32 +0000

    ejrydf

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,15 +3,55 @@ from transformers import GPT2Config, GPT2LMHeadModel, PreTrainedTokenizerFast, D
 import gdown
 import torch
 
+
+def generate_text(prompt, max_length=50, temperature=0.3, top_k=50, top_p=0.9, my_model, myTokenizer):
+    # Encode the input prompt
+    input_ids = myTokenizer.encode(prompt, return_tensors='pt').to(my_model.device)
+    attention_mask = torch.ones_like(input_ids)
+    # Generate text
+    output = my_model.generate(
+        input_ids,
+        max_length=max_length,
+        temperature=temperature,
+        top_k=top_k,
+        top_p=top_p,
+        do_sample=True,
+        num_return_sequences=1,
+        attention_mask=attention_mask,
+        pad_token_id=myTokenizer.eos_token_id
+    )
+
+    # Decode the generated text
+    generated_text = myTokenizer.decode(output[0], skip_special_tokens=False)
+    return generated_text
+
 # Show title and description.
 st.title("Panini Parser")
+st.write("Ask anything to the chatbot!")
 
 if "model" not in st.session_state:
     url = "https://drive.google.com/file/d/1-kU1fY8LdiNa-osQwpaGO43vB3LN88-7/view?usp=drive_link"
-    output = "model.bin"
+    output = "model.pth"
     gdown.download(url, output, quiet=False)
-    st.session_state.model = output
-    st.rerun()
+    config = GPT2Config(vocab_size=65536,n_head=16)
+    st.session_state.model = GPT2LMHeadModel(config)
+    model_save_path = output
+    st.session_state.model.load_state_dict(torch.load(model_save_path))
 
-else: 
-    st.write("hello")
+if "token" not in st.session_state:
+
+    tokenizer_path = 'fine_tuned_tokenizer.json'
+    st.session_state.tokenizer = PreTrainedTokenizerFast(tokenizer_file = tokenizer_path)
+
+    if st.session_state.tokenizer.pad_token is None:
+        if st.session_state.tokenizer.eos_token is not None:
+            st.session_state.tokenizer.pad_token = st.session_state.tokenizer.eos_token
+        else:
+            st.session_state.tokenizer.add_special_tokens({'pad_token': '[PAD]'})
+
+user_input = st.text_input("You:", placeholder="Type your message here...")
+
+if user_input:
+    with st.spinner("Generating response..."):
+        response = generate_text(user_input, max_length=50, temperature=0.3, top_k=50, top_p=0.9, st.session_state.model, st.session_state.tokenizer)
+        st.write(f"Chatbot: {response}")
