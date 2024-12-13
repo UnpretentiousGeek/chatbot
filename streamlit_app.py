4e69e01352d9f91e2e52a24bd6556d64d76e97f7
Sanchit Tomar <85986502+UnpretentiousGeek@users.noreply.github.com>
2024-12-13 05:03:04 +0000

    ejyrd

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,8 +8,7 @@ st.title("Panini Parser")
 
 if "model" not in st.session_state:
     url = "https://drive.google.com/file/d/1-kU1fY8LdiNa-osQwpaGO43vB3LN88-7/view?usp=drive_link"
-    output = st.session_state.model
-    gdown.download(url, output, quiet=False)
+    gdown.download(url, st.session_state.model, quiet=False)
 
 else: 
     st.write("hello")
